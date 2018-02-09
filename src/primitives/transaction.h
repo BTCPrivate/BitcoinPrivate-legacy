67602acbebfade1aef0ca1ec7cd7c2dbed85b52b
Inga Bemman (R0otChiXor) <R0otChiXor@gmail.com>
2018-02-08 22:09:36 -0700

    added stdint.h

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -1,17 +1,18 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2014 The Bitcoin Core developers
+// Copyright (c) 2009-2017 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_PRIMITIVES_TRANSACTION_H
 #define BITCOIN_PRIMITIVES_TRANSACTION_H
 
-#include "amount.h"
-#include "random.h"
-#include "script/script.h"
-#include "serialize.h"
-#include "uint256.h"
-#include "consensus/consensus.h"
+#include <stdint.h>
+#include <amount.h>
+#include <random.h>
+#include <script/script.h>
+#include <serialize.h>
+#include <uint256.h>
+#include <consensus/consensus.h>
 
 #include <boost/array.hpp>
 
