06b818d78546d1fb63cd91f78aac9ce7c4787ab7
Jon Layton <jon@seequ.com>
2018-03-02 21:58:39 -0600

    BTCP server stopped

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -245,10 +245,10 @@ UniValue stop(const UniValue& params, bool fHelp)
     if (fHelp || params.size() > 1)
         throw runtime_error(
             "stop\n"
-            "\nStop Zcash server.");
+            "\nStop BTCP server.");
     // Shutdown will take long enough that the response should get back
     StartShutdown();
-    return "Zcash server stopping";
+    return "BTCP server stopping";
 }
 
 /**
