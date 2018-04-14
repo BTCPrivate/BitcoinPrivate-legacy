29fc68b4ce27fd1f302c8aba7f9ec2757e2b8727
Don Shin <don.shin@crosscomm.net>
2018-04-14 08:32:54 -0500

    Incremented protocol version to 180004 to match recent changes with segwit claiming.

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 180003;
+static const int PROTOCOL_VERSION = 180004;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
