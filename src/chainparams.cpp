7ef59740894945be316106d335b091ab691dfc7a
CryptoJake22 <34906725+BlueSilver22@users.noreply.github.com>
2018-04-27 18:32:42 -0400

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -104,8 +104,9 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
         // TODO: setup a DNSSeed
+        vSeeds.push_back(CDNSSeedData("btcprivate.org", "dnsseed.btcprivate.org"));
         vSeeds.push_back(CDNSSeedData("btcprivate.co", "dnsseed.btcprivate.co"));
-
+        
         // guarantees the first 2 characters, when base58 encoded, are "b1"
         base58Prefixes[PUBKEY_ADDRESS]     = {0x13,0x25};
         // guarantees the first 2 characters, when base58 encoded, are "bx"
@@ -200,6 +201,7 @@ public:
 
         vFixedSeeds.clear();
         vSeeds.clear();
+        vSeeds.push_back(CDNSSeedData("btcprivate.org", "dnsseed.testnet.btcprivate.org"));
         vSeeds.push_back(CDNSSeedData("btcprivate.co", "dnsseed.testnet1.btcprivate.co"));
 
         // guarantees the first 2 characters, when base58 encoded, are "n1"
