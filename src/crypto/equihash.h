3ca363cd4447876de25777c33a8c24a54ef16652
michaelotis <34221002+michaelotis@users.noreply.github.com>
2018-05-09 16:32:46 -0500

    Update equihash.h

--- src/crypto/equihash.h
+++ src/crypto/equihash.h
@@ -201,12 +201,15 @@ static Equihash<200,9> Eh200_9;
 static Equihash<96,5> Eh96_5;
 static Equihash<48,5> Eh48_5;
 static Equihash<144,5> Eh144_5;
+static Equihash<192,7> Eh192_7;
 
 #define EhInitialiseState(n, k, base_state)  \
     if (n == 96 && k == 3) {                 \
         Eh96_3.InitialiseState(base_state);  \
     } else if (n == 200 && k == 9) {         \
         Eh200_9.InitialiseState(base_state); \
+    } else if (n == 192 && k == 7) {         \
+        Eh192_7.InitialiseState(base_state); \
     } else if (n == 144 && k == 5) {         \
         Eh144_5.InitialiseState(base_state); \
     } else if (n == 96 && k == 5) {          \
@@ -226,6 +229,8 @@ inline bool EhBasicSolve(unsigned int n, unsigned int k, const eh_HashState& bas
         return Eh96_3.BasicSolve(base_state, validBlock, cancelled);
     } else if (n == 200 && k == 9) {
         return Eh200_9.BasicSolve(base_state, validBlock, cancelled);
+    } else if (n == 192 && k == 7) {
+        return Eh144_5.BasicSolve(base_state, validBlock, cancelled);
     } else if (n == 144 && k == 5) {
         return Eh144_5.BasicSolve(base_state, validBlock, cancelled);
     } else if (n == 96 && k == 5) {
@@ -252,6 +257,8 @@ inline bool EhOptimisedSolve(unsigned int n, unsigned int k, const eh_HashState&
         return Eh96_3.OptimisedSolve(base_state, validBlock, cancelled);
     } else if (n == 200 && k == 9) {
         return Eh200_9.OptimisedSolve(base_state, validBlock, cancelled);
+    } else if (n == 192 && k == 7) {
+        return Eh192_7.OptimisedSolve(base_state, validBlock, cancelled);
     } else if (n == 144 && k == 5) {
         return Eh144_5.OptimisedSolve(base_state, validBlock, cancelled);
     } else if (n == 96 && k == 5) {
@@ -276,6 +283,8 @@ inline bool EhOptimisedSolveUncancellable(unsigned int n, unsigned int k, const 
         ret = Eh96_3.IsValidSolution(base_state, soln);  \
     } else if (n == 200 && k == 9) {                     \
         ret = Eh200_9.IsValidSolution(base_state, soln); \
+    } else if (n == 192 && k == 7) {                     \
+        ret = Eh192_7.IsValidSolution(base_state, soln); \
     } else if (n == 144 && k == 5) {                     \
         ret = Eh144_5.IsValidSolution(base_state, soln); \
     } else if (n == 96 && k == 5) {                      \
@@ -293,6 +302,8 @@ inline unsigned int EhSolutionWidth(int n, int k)
         ret = Eh96_3.SolutionWidth;
     } else if (n == 200 && k == 9) {
         ret = Eh200_9.SolutionWidth;
+    } else if (n == 192 && k == 7) {
+        ret = Eh192_7.SolutionWidth;
     } else if (n == 144 && k == 5) {
         ret = Eh144_5.SolutionWidth;
     } else if (n == 96 && k == 5) {
