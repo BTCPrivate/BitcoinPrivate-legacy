519ceaa3674faf07376f6ec14be86c692513518d
BitcoinPr1vate <46205251+BitcoinPr1vate@users.noreply.github.com>
2018-12-29 12:39:55 -0800

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -59,7 +59,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999; // December 31, 2008
-        consensus.nUnmovedBurnHeight = 475000;
+        consensus.nUnmovedBurnHeight = 480000;
         consensus.zResetHeight = 455500;
 
         pchMessageStart[0] = 0xa8;
