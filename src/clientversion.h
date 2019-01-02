53e4647b0d61432a5d28c934b3c7c64db77192d2
J62 <j.owens@interbiznw.com>
2019-01-01 21:04:55 -0800

    bump version and update copyright year to 2019 (#199)
    * Bump version and copyright year
    
    * Bump version and copyright year
    
    * Bump version
    
    * Update README.md

--- src/clientversion.h
+++ src/clientversion.h
@@ -19,8 +19,8 @@
 //! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR 1
 #define CLIENT_VERSION_MINOR 0
-#define CLIENT_VERSION_REVISION 12
-#define CLIENT_VERSION_BUILD 51
+#define CLIENT_VERSION_REVISION 13
+#define CLIENT_VERSION_BUILD 50
 
 //! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE true
@@ -29,7 +29,7 @@
  * Copyright year (2009-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2018
+#define COPYRIGHT_YEAR 2019
 
 #endif //HAVE_CONFIG_H
 
