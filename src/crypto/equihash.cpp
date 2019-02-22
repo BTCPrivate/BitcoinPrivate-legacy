0b0a39661c7b14418e8709f0eef463908a73ed37
J62 <j.owens@interbiznw.com>
2019-02-22 00:33:24 -0800

    Fixed pers. to be dependent on N & K values

--- src/crypto/equihash.cpp
+++ src/crypto/equihash.cpp
@@ -36,7 +36,11 @@ int Equihash<N,K>::InitialiseState(eh_HashState& base_state)
     uint32_t le_N = htole32(N);
     uint32_t le_K = htole32(K);
     unsigned char personalization[crypto_generichash_blake2b_PERSONALBYTES] = {};
-    memcpy(personalization, "BTCP_PoW", 8);
+
+    if(N==192 && K==7)
+        memcpy(personalization, "BTCP_PoW", 8);
+    else
+        memcpy(personalization, "ZcashPoW", 8);
     memcpy(personalization+8,  &le_N, 4);
     memcpy(personalization+12, &le_K, 4);
     return crypto_generichash_blake2b_init_salt_personal(&base_state,
