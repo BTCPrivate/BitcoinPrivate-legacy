bade413aab4f0b17221ae8816c676e112fb38487
J62 <j.owens@interbiznw.com>
2019-02-22 18:14:32 -0800

    Change nPowAveragingWindow

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -48,7 +48,7 @@ public:
         consensus.nMajorityWindow = 4000;
         consensus.prePowLimit = uint256S("0007ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.powLimit = uint256S("07ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-        consensus.nPowAveragingWindow = 17;
+        consensus.nPowAveragingWindow = 40;
         assert(maxUint/UintToArith256(consensus.powLimit) >= consensus.nPowAveragingWindow);
         consensus.nPowMaxAdjustDown = 32; // 32% adjustment down
         consensus.nPowMaxAdjustUp = 16; // 16% adjustment up
@@ -179,7 +179,6 @@ public:
         consensus.nMajorityWindow = 400;
         consensus.powLimit = uint256S("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.prePowLimit = consensus.powLimit;
-        consensus.nPowAveragingWindow = 40;
         //assert(maxUint/UintToArith256(consensus.powLimit) >= consensus.nPowAveragingWindow);
         consensus.fPowAllowMinDifficultyBlocks = true;
 
