1d8ed84f3aa4051dd3e65c2e50ce1ff91be7e99d
J62 <j.owens@interbiznw.com>
2019-09-17 16:51:34 -0700

    Added alternate dns seeder
    Added alternate DNS seeder ran and maintained by myself located at dnsseed.interbiznw.com. Latest DNS seeder code ran on an actively maintained BTCP node to ensure that the seeder only seeds the correct up to date nodes.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -110,6 +110,7 @@ public:
         // TODO: setup a DNSSeed
         vSeeds.push_back(CDNSSeedData("btcprivate.org", "dnsseed.btcprivate.org"));
         vSeeds.push_back(CDNSSeedData("btcprivate.co", "dnsseed.btcprivate.co"));
+        vSeeds.push_back(CDNSSeedData("interbiznw.com", "dnsseed.interbiznw.com")); // dev run seeder by "J62"
 
         // guarantees the first 2 characters, when base58 encoded, are "b1"
         base58Prefixes[PUBKEY_ADDRESS]     = {0x13,0x25};
