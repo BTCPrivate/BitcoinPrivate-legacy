76b9672c0f38f9093345734fc6ae5e4a59810798
J62 <j.owens@interbiznw.com>
2019-10-13 01:48:59 -0700

    --disable-tests

--- btcputil/build-mac.sh
+++ btcputil/build-mac.sh
@@ -87,6 +87,6 @@ NO_RUST="$RUST_ARG" NO_PROTON="$PROTON_ARG" make "$@" -C ./depends/ V=1 NO_QT=1
 ./autogen.sh
 CPPFLAGS="-I$PREFIX/include -arch x86_64" LDFLAGS="-L$PREFIX/lib -arch x86_64 -Wl,-no_pie" \
 CXXFLAGS='-arch x86_64 -I/usr/local/Cellar/gcc5/5.4.0/include/c++/5.4.0 -I$PREFIX/include -fwrapv -fno-strict-aliasing -Werror -g -Wl,-undefined -Wl,dynamic_lookup' \
-./configure --prefix="${PREFIX}" --with-gui=no "$RUST_ARG" "$HARDENING_ARG" "$LCOV_ARG" "$TEST_ARG" "$MINING_ARG" "$PROTON_ARG"
+./configure --prefix="${PREFIX}" --with-gui=no --disable-tests "$RUST_ARG" "$HARDENING_ARG" "$LCOV_ARG" "$TEST_ARG" "$MINING_ARG" "$PROTON_ARG"
 
 make "$@" V=1 NO_GTEST=0 STATIC=1
