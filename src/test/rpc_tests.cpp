de81219400b63130b22db9c37576a5f7bb80d4e6
J62 <j.owens@interbiznw.com>
2019-10-20 00:07:51 -0700

    Fix parsing parameters in getnetworksolps

--- src/test/rpc_tests.cpp
+++ src/test/rpc_tests.cpp
@@ -295,4 +295,11 @@ BOOST_AUTO_TEST_CASE(rpc_ban)
     BOOST_CHECK_EQUAL(adr.get_str(), "2001:4d48:ac57:400:cacf:e9ff:fe1d:9c63/ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff");
 }
 
+BOOST_AUTO_TEST_CASE(rpc_getnetworksolps)
+{
+    BOOST_CHECK_NO_THROW(CallRPC("getnetworksolps"));
+    BOOST_CHECK_NO_THROW(CallRPC("getnetworksolps 120"));
+    BOOST_CHECK_NO_THROW(CallRPC("getnetworksolps 120 -1"));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
